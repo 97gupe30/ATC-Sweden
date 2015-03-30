b68824dca0ab9c8f6fff006f21560fc3a3f4229c
Vezel <Gustav@Gustavs-Air.zyxel.com>
2015-03-30 18:55:12 +0200

    Swag
    g

--- script/main.js
+++ script/main.js
@@ -1,6 +1,8 @@
 var aircraftCount = 0;
 var companies = ['NAX', 'BAW', 'EZY', 'SAS', 'KLM'];
-var aircrafts = [new Aircraft(100, 100, '', 5000, 2, aircraftCount), new Aircraft(150, 120, '', 8000, 6, (aircraftCount + 1))];
+var aircrafts = [new Aircraft(100, 100, '', 8000, 2, aircraftCount), new Aircraft(150, 120, '', 8000, 6, (aircraftCount + 1))];
+var waypoints = [new WayPoint(200, 100, 10, 'GOLUM', 'Jonathan'),
+                 new WayPoint(300, 400, 10, 'Jonathan', 'df')];
 aircraftCount++;
 
 var vinkel, x, y;
@@ -16,18 +18,47 @@ function init() {
     game.addEventListener("mousedown", mouseClick, false);
 }
 
-function keyHandler(event, type) {
+function keyHandler(event) {
+    event.preventDefault();
     var key = event.keyCode;
     if(key == 27) {
         for(var i = 0; i < aircrafts.length; i++) {
             aircrafts[i].activeMenu = 'none';
         }
     }
-    if(type == 'input') {
-        if(key == 13) {
-            event.preventDefault();
+    for(var i = 0; i < aircrafts.length; i++) {
+
+        if(aircrafts[i].activeMenu == 'hdgSelect') {
+            if(key == 38 && aircrafts[i].tempAlt < 41000 && aircrafts[i].tempAlt >= 0) {
+                aircrafts[i].tempAlt += 1000;
+            } else if(key == 40 && aircrafts[i].tempAlt > 0 && aircrafts[i].tempAlt <= 41000) {
+                aircrafts[i].tempAlt -= 1000;
+            } else if(key == 39 && aircrafts[i].tempSpeed < 450 && aircrafts[i].tempSpeed >= 0) {
+                aircrafts[i].tempSpeed += 10;
+            } else if(key == 37 && aircrafts[i].tempSpeed > 0 && aircrafts[i].tempSpeed <= 450) {
+                aircrafts[i].tempSpeed -= 10;
+            } else if(key == 13) {
+                aircrafts[i].finalAlt = aircrafts[i].tempAlt;
+                aircrafts[i].finalSpeed = aircrafts[i].tempSpeed;
+            }
         }
     }
+}
+
+function WayPoint(x, y, w, name, next) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.name = name;
+    this.nextWaypoint = next;
+
+    this.render = function() {
+        ctx.font = "10px Arial";
+        ctx.strokeRect(this.x, this.y, this.w, this.w);
+        ctx.strokeRect(this.x + 3, this.y + 3, this.w - 6, this.w - 6);
+        ctx.fillText(this.name.toUpperCase(), this.x - this.name.length * 5 / 2, this.y + 20);
+    }
+
 }
 
 function Aircraft(x, y, hdg, altitude, speed, id) {
@@ -35,17 +66,19 @@ function Aircraft(x, y, hdg, altitude, speed, id) {
     this.y = y;
     this.speed = speed;
     this.trueSpeed = Math.abs(speed) * 60;
-    this.accSpeed = Math.random() * 5 // Hur snabbt planet accelererar
-    while(this.accSpeed < 1 || this.accSpeed > 4) {
+    this.accSpeed = Math.random() * 7 // Hur snabbt planet accelererar
+    while(this.accSpeed < 1 || this.accSpeed > 6) {
         this.accSpeed = Math.random() * 5
     }
     this.finalSpeed = this.trueSpeed;
-    this.vSpeed = Math.random() * 3000;
+    this.tempSpeed = this.trueSpeed;
+    this.vSpeed = Math.random() * 4000;
     while(this.vSpeed < 1200 || this.vSpeed > 2800) {
         this.vSpeed = Math.random() * 3000;
     }
     this.altitude = altitude;
-    this.finalAlt = (this.altitude + 1000);
+    this.finalAlt = this.altitude;
+    this.tempAlt = this.altitude;
     this.vx = speed;
     this.vy = speed;
     this.hdg = hdg;
@@ -75,7 +108,7 @@ function Aircraft(x, y, hdg, altitude, speed, id) {
         ctx.beginPath();
         ctx.fillRect(this.x, this.y, 5, 5);
         ctx.closePath();
-        
+
         // Denna delen av koden kollar om planet ska descenda, climba, accelerera eller deaccelerera och gör det isåfall.
         if(this.finalAlt < this.altitude) { // Descend eller climb
             this.altitude -= (this.vSpeed / 3000);
@@ -104,10 +137,19 @@ function Aircraft(x, y, hdg, altitude, speed, id) {
 }
 
 function Menu() {
-    this.activeMenu = 'none';
     this.hdgSelect = function(i) {
-        ctx.arc(aircrafts[i].x + 5, aircrafts[i].y + 5, 100, 0, 2 * Math.PI); // HÄR SKA DU FIXA NU!
+        ctx.arc(aircrafts[i].x + 5, aircrafts[i].y + 5, 100, 0, 2 * Math.PI);
+        ctx.strokeRect(aircrafts[i].x - 110, aircrafts[i].y - 70, 10, 150);
+        ctx.fillRect(aircrafts[i].x - 110, aircrafts[i].y + 80, 10, -aircrafts[i].tempAlt / 274);
+        ctx.strokeRect(aircrafts[i].x - 70, aircrafts[i].y + 110, 150, 10);
+        ctx.fillRect(aircrafts[i].x - 70, aircrafts[i].y + 110, aircrafts[i].tempSpeed / 3.01, 10);
         ctx.stroke();
+
+        var altX = aircrafts[i].x - 135;
+        if( aircrafts[i].tempAlt / 100 >= 100)
+            altX -= 6;
+        ctx.fillText("FL" + aircrafts[i].tempAlt / 100, altX, aircrafts[i].y - aircrafts[i].tempAlt / 300 + 83);
+        ctx.fillText(aircrafts[i].tempSpeed + "kt", aircrafts[i].x + aircrafts[i].tempSpeed / 3 - 70, aircrafts[i].y + 130);
     }
 }
 
@@ -117,8 +159,8 @@ function calculate(event) {
             var rect = game.getBoundingClientRect();
             x = event.clientX - rect.left;
             y = event.clientY - rect.top;
-            var dx = aircrafts[i].x + 5 - x;
-            var dy = y - aircrafts[i].y + 5;
+            var dx = aircrafts[i].x + 2.5 - x;
+            var dy = y - aircrafts[i].y - 2.5;
             vinkel = Math.round(Math.atan(dx / dy) * (180/Math.PI));
             if(dx <= 0 && dy >= 0) {
                 vinkel += 180;
@@ -130,7 +172,7 @@ function calculate(event) {
             if(vinkel < 0) {
                 vinkel = Math.abs(vinkel);
             }
-            
+
         }
     }
 
@@ -168,11 +210,41 @@ function mouseClick(event) {
     }
 }
 
+function crashDetect() {
+    for(var i = 0; i < aircrafts.length; i++) {
+        for(var j = 0; j < aircrafts.length; j++) {
+            if(j != i) {
+                if(aircrafts[i].x >= aircrafts[j].x && aircrafts[i].x <= aircrafts[j].x + 5 && aircrafts[i].y >= aircrafts[j].y && aircrafts[i].y <= aircrafts[j].y + 5 ||
+                   aircrafts[i].x + 5 >= aircrafts[j].x && aircrafts[i].x + 5 <= aircrafts[j].x + 5 && aircrafts[i].y >= aircrafts[j].y && aircrafts[i].y <= aircrafts[j].y + 5 ||
+                   aircrafts[i].x >= aircrafts[j].x && aircrafts[i].x <= aircrafts[j].x + 5 && aircrafts[i].y + 5 >= aircrafts[j].y && aircrafts[i].y + 5 <= aircrafts[j].y + 5 ||
+                   aircrafts[i].x + 5 >= aircrafts[j].x && aircrafts[i].x + 5 <= aircrafts[j].x + 5 && aircrafts[i].y + 5 >= aircrafts[j].y && aircrafts[i].y + 5 <= aircrafts[j].y + 5) {
+                    if(aircrafts[i].altitude >= aircrafts[j].altitude - 100 && aircrafts[i].altitude <= aircrafts[j].altitude + 100) {
+                        console.log("CRASH");
+                    }
+                }
+            }
+        }
+    }
+    
+    for(var i = 0; i < aircrafts.length; i++) {
+        for(var j = 0; j < waypoints.length; j++) {
+            if(aircrafts[i].x >= waypoints[j].x && aircrafts[i].x <= waypoints[j].x + waypoints[j].w && aircrafts[i].y >= waypoints[j].y && aircrafts[i].y <= waypoints[j].y + waypoints[j].w) {
+                console.log("WAYPOINT");
+            }
+        }
+    }
+}
+
 function update() {
     ctx.clearRect(0, 0, 1280, 720);
+    crashDetect();
+    for(var i = 0; i < waypoints.length; i++) {
+        waypoints[i].render();
+    }
+
     for(var i = 0; i < aircrafts.length; i++) {
         aircrafts[i].trueSpeed = Math.abs(aircrafts[i].speed) * 60;
-        
+
         var height;
         if(aircrafts[i].altitude >= 7000) {
             height = "FL" + Math.round(aircrafts[i].altitude / 100);
